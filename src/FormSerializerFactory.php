f4fe783bbb8b3f74d93a2addf08de2002e03e296
MartkCz <martk494@gmail.com>
2021-01-09 12:49:14 +0100

    FormSerializerFactory inject validator

--- src/FormSerializerFactory.php
+++ src/FormSerializerFactory.php
@@ -5,6 +5,7 @@ namespace WebChemistry\FormSerializer;
 use Doctrine\ORM\EntityManagerInterface;
 use Nette\Application\UI\Form;
 use Symfony\Component\Serializer\Serializer;
+use WebChemistry\Validator\Validator;
 
 final class FormSerializerFactory implements FormSerializerFactoryInterface
 {
@@ -12,13 +13,14 @@ final class FormSerializerFactory implements FormSerializerFactoryInterface
 	public function __construct(
 		private Serializer $serializer,
 		private EntityManagerInterface $em,
+		private ?Validator $validator = null,
 	)
 	{
 	}
 
 	public function create(Form $form, string $class): FormSerializer
 	{
-		$serializer = new FormSerializer($form, $class, $this->serializer, $this->em);
+		$serializer = new FormSerializer($form, $class, $this->serializer, $this->em, $this->validator);
 
 		if ($form instanceof FormSerializerAwareInterface) {
 			$form->setSerializer($serializer);
